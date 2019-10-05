0fb897d7d18be6658b2fad7b31c6e8fd30abe39e
UIS-E3T <comdiguis@saber.uis.edu.co>
2019-10-05 17:23:55 -0500

    codigo

--- Flujogramas_Compartidos/top_block.py
+++ Flujogramas_Compartidos/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Aug 22 09:14:27 2019
+# Generated: Tue Oct  1 20:47:59 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -20,16 +20,13 @@ from PyQt4 import Qt
 from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
-import  math
-import E3TRadio
-import VCO_hob
-import numpy as np
+import epy_block_0
+import epy_block_0_0
 import sip
 import sys
 from gnuradio import qtgui
@@ -66,140 +63,88 @@ class top_block(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 32000
-        self.run_stop = run_stop = True
-        self.N = N = 256
 
         ##################################################
         # Blocks
         ##################################################
-        _run_stop_check_box = Qt.QCheckBox('Inicial/Parar')
-        self._run_stop_choices = {True: True, False: False}
-        self._run_stop_choices_inv = dict((v,k) for k,v in self._run_stop_choices.iteritems())
-        self._run_stop_callback = lambda i: Qt.QMetaObject.invokeMethod(_run_stop_check_box, "setChecked", Qt.Q_ARG("bool", self._run_stop_choices_inv[i]))
-        self._run_stop_callback(self.run_stop)
-        _run_stop_check_box.stateChanged.connect(lambda i: self.set_run_stop(self._run_stop_choices[bool(i)]))
-        self.top_grid_layout.addWidget(_run_stop_check_box, 0, 0, 1, 1)
-        for r in range(0, 1):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
-        self.qtgui_vector_sink_f_0_0_0 = qtgui.vector_sink_f(
-            N,
+        self.qtgui_number_sink_0_0 = qtgui.number_sink(
+            gr.sizeof_float,
             0,
-            1.0,
-            "time",
-            "amplitude",
-            "sample functions",
-            10 # Number of inputs
+            qtgui.NUM_GRAPH_HORIZ,
+            1
         )
-        self.qtgui_vector_sink_f_0_0_0.set_update_time(0.10)
-        self.qtgui_vector_sink_f_0_0_0.set_y_axis(-1.5, 1.5)
-        self.qtgui_vector_sink_f_0_0_0.enable_autoscale(False)
-        self.qtgui_vector_sink_f_0_0_0.enable_grid(False)
-        self.qtgui_vector_sink_f_0_0_0.set_x_axis_units("")
-        self.qtgui_vector_sink_f_0_0_0.set_y_axis_units("")
-        self.qtgui_vector_sink_f_0_0_0.set_ref_level(0)
-
-        labels = ['sample function 0', 'sample  function1', 'sample function 2', 'sample function 3', 'sample function 4',
-                  'sample function 5', 'sample function 6', 'sample function 7', 'sample function 8', 'sample function 9']
-        widths = [3, 3, 3, 3, 3,
-                  3, 3, 3, 3, 3]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(10):
-            if len(labels[i]) == 0:
-                self.qtgui_vector_sink_f_0_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_vector_sink_f_0_0_0.set_line_label(i, labels[i])
-            self.qtgui_vector_sink_f_0_0_0.set_line_width(i, widths[i])
-            self.qtgui_vector_sink_f_0_0_0.set_line_color(i, colors[i])
-            self.qtgui_vector_sink_f_0_0_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_vector_sink_f_0_0_0_win = sip.wrapinstance(self.qtgui_vector_sink_f_0_0_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_vector_sink_f_0_0_0_win)
-        self.qtgui_vector_sink_f_0 = qtgui.vector_sink_f(
-            N,
-            0,
-            1.0,
-            "time",
-            "amplitude",
-            "mean function",
-            1 # Number of inputs
-        )
-        self.qtgui_vector_sink_f_0.set_update_time(0.10)
-        self.qtgui_vector_sink_f_0.set_y_axis(-0.004, 0.004)
-        self.qtgui_vector_sink_f_0.enable_autoscale(False)
-        self.qtgui_vector_sink_f_0.enable_grid(False)
-        self.qtgui_vector_sink_f_0.set_x_axis_units("")
-        self.qtgui_vector_sink_f_0.set_y_axis_units("")
-        self.qtgui_vector_sink_f_0.set_ref_level(0)
+        self.qtgui_number_sink_0_0.set_update_time(0.10)
+        self.qtgui_number_sink_0_0.set_title("The Mean Power")
 
         labels = ['', '', '', '', '',
                   '', '', '', '', '']
-        widths = [3, 1, 1, 1, 1,
+        units = ['', '', '', '', '',
+                 '', '', '', '', '']
+        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+                  ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [1, 1, 1, 1, 1,
                   1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
         for i in xrange(1):
+            self.qtgui_number_sink_0_0.set_min(i, -1)
+            self.qtgui_number_sink_0_0.set_max(i, 1)
+            self.qtgui_number_sink_0_0.set_color(i, colors[i][0], colors[i][1])
             if len(labels[i]) == 0:
-                self.qtgui_vector_sink_f_0.set_line_label(i, "Data {0}".format(i))
+                self.qtgui_number_sink_0_0.set_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_vector_sink_f_0.set_line_label(i, labels[i])
-            self.qtgui_vector_sink_f_0.set_line_width(i, widths[i])
-            self.qtgui_vector_sink_f_0.set_line_color(i, colors[i])
-            self.qtgui_vector_sink_f_0.set_line_alpha(i, alphas[i])
+                self.qtgui_number_sink_0_0.set_label(i, labels[i])
+            self.qtgui_number_sink_0_0.set_unit(i, units[i])
+            self.qtgui_number_sink_0_0.set_factor(i, factor[i])
 
-        self._qtgui_vector_sink_f_0_win = sip.wrapinstance(self.qtgui_vector_sink_f_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_vector_sink_f_0_win)
-        self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_fff(N, (np.array([1]*N)))
-        self.interp_fir_filter_xxx_0.declare_sample_delay(0)
-        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_float*1, N)
-        self.blocks_stream_to_streams_0 = blocks.stream_to_streams(gr.sizeof_float*N, 10)
-        self.blocks_null_sink_0_1 = blocks.null_sink(gr.sizeof_float*1)
-        self.blocks_null_sink_0_0 = blocks.null_sink(gr.sizeof_float*1)
-        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
-        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_vff((math.pi, ))
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((1/1000., ))
-        self.blocks_int_to_float_0 = blocks.int_to_float(1, 1)
-        self.analog_random_uniform_source_x_0 = analog.random_uniform_source_i(-1000, 1001, 24)
+        self.qtgui_number_sink_0_0.enable_autoscale(False)
+        self._qtgui_number_sink_0_0_win = sip.wrapinstance(self.qtgui_number_sink_0_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_number_sink_0_0_win)
+        self.qtgui_number_sink_0 = qtgui.number_sink(
+            gr.sizeof_float,
+            0,
+            qtgui.NUM_GRAPH_HORIZ,
+            1
+        )
+        self.qtgui_number_sink_0.set_update_time(0.10)
+        self.qtgui_number_sink_0.set_title("The mean")
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        units = ['', '', '', '', '',
+                 '', '', '', '', '']
+        colors = [("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"),
+                  ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black"), ("black", "black")]
+        factor = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        for i in xrange(1):
+            self.qtgui_number_sink_0.set_min(i, -1)
+            self.qtgui_number_sink_0.set_max(i, 1)
+            self.qtgui_number_sink_0.set_color(i, colors[i][0], colors[i][1])
+            if len(labels[i]) == 0:
+                self.qtgui_number_sink_0.set_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_number_sink_0.set_label(i, labels[i])
+            self.qtgui_number_sink_0.set_unit(i, units[i])
+            self.qtgui_number_sink_0.set_factor(i, factor[i])
+
+        self.qtgui_number_sink_0.enable_autoscale(False)
+        self._qtgui_number_sink_0_win = sip.wrapinstance(self.qtgui_number_sink_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_number_sink_0_win)
+        self.epy_block_0_0 = epy_block_0_0.mean_meter()
+        self.epy_block_0 = epy_block_0.mean_meter()
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
         self.analog_noise_source_x_0 = analog.noise_source_f(analog.GR_GAUSSIAN, 1, 0)
-        self.analog_const_source_x_0_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 400000)
-        self.VCO_hob = VCO_hob.blk()
-        self.E3TRadio_vector_average_hob_0 = E3TRadio.vector_average_hob(N, 100000000)
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.E3TRadio_vector_average_hob_0, 0), (self.qtgui_vector_sink_f_0, 0))
-        self.connect((self.VCO_hob, 0), (self.blocks_null_sink_0, 0))
-        self.connect((self.VCO_hob, 0), (self.blocks_stream_to_vector_0, 0))
-        self.connect((self.analog_const_source_x_0_0, 0), (self.VCO_hob, 1))
-        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_null_sink_0_1, 0))
-        self.connect((self.analog_random_uniform_source_x_0, 0), (self.blocks_int_to_float_0, 0))
-        self.connect((self.blocks_int_to_float_0, 0), (self.blocks_multiply_const_vxx_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.interp_fir_filter_xxx_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.VCO_hob, 2))
-        self.connect((self.blocks_stream_to_streams_0, 0), (self.qtgui_vector_sink_f_0_0_0, 0))
-        self.connect((self.blocks_stream_to_streams_0, 1), (self.qtgui_vector_sink_f_0_0_0, 1))
-        self.connect((self.blocks_stream_to_streams_0, 2), (self.qtgui_vector_sink_f_0_0_0, 2))
-        self.connect((self.blocks_stream_to_streams_0, 3), (self.qtgui_vector_sink_f_0_0_0, 3))
-        self.connect((self.blocks_stream_to_streams_0, 4), (self.qtgui_vector_sink_f_0_0_0, 4))
-        self.connect((self.blocks_stream_to_streams_0, 5), (self.qtgui_vector_sink_f_0_0_0, 5))
-        self.connect((self.blocks_stream_to_streams_0, 6), (self.qtgui_vector_sink_f_0_0_0, 6))
-        self.connect((self.blocks_stream_to_streams_0, 7), (self.qtgui_vector_sink_f_0_0_0, 7))
-        self.connect((self.blocks_stream_to_streams_0, 8), (self.qtgui_vector_sink_f_0_0_0, 8))
-        self.connect((self.blocks_stream_to_streams_0, 9), (self.qtgui_vector_sink_f_0_0_0, 9))
-        self.connect((self.blocks_stream_to_vector_0, 0), (self.E3TRadio_vector_average_hob_0, 0))
-        self.connect((self.blocks_stream_to_vector_0, 0), (self.blocks_stream_to_streams_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.VCO_hob, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_null_sink_0_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.analog_noise_source_x_0, 0), (self.epy_block_0_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.epy_block_0, 0))
+        self.connect((self.epy_block_0, 0), (self.qtgui_number_sink_0_0, 0))
+        self.connect((self.epy_block_0_0, 0), (self.qtgui_number_sink_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -211,22 +156,6 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-
-    def get_run_stop(self):
-        return self.run_stop
-
-    def set_run_stop(self, run_stop):
-        self.run_stop = run_stop
-        if self.run_stop: self.start()
-        else: self.stop(); self.wait()
-        self._run_stop_callback(self.run_stop)
-
-    def get_N(self):
-        return self.N
-
-    def set_N(self, N):
-        self.N = N
-        self.interp_fir_filter_xxx_0.set_taps((np.array([1]*self.N)))
 
 
 def main(top_block_cls=top_block, options=None):
